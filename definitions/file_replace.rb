f01ad5158cb8e5e9f1147be2eea3054615457f30
Lloyd Philbrook <lloyd@tablexi.com>
2013-12-16 17:43:53 +0800

    should have actually checked the syntax

--- definitions/file_replace.rb
+++ definitions/file_replace.rb
@@ -1,16 +1,17 @@
 define :file_replace do
   file_path = params[:path] || params[:name]
+  replace_regex = Regexp.escape(params[:replace])
 
 	ruby_block "#{params[:name]}" do
 		block do
 			file = Chef::Util::FileEdit.new(file_path)
 			file.search_file_replace(
-				/#{params[:replace]}/,
+				/#{replace_regex}/,
 				(params[:with] or '')
 			)
 			file.write_file
 		end
-		only_if { ::File.read(file_path) =~ /#{Regexp.escape(params[:replace]})/ }
+		only_if { ::File.read(file_path) =~ /#{replace_regex}/ }
 		
 		# Notify listener
 		if params[:notifies]
