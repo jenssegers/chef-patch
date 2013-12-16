8fb2f6097dcc031982ca0acd0ba3b8eada0773a1
Lloyd Philbrook <lloyd@tablexi.com>
2013-12-16 17:57:50 +0800

    when regex is passed don't try to escape it

--- definitions/file_replace.rb
+++ definitions/file_replace.rb
@@ -1,17 +1,17 @@
 define :file_replace do
   file_path = params[:path] || params[:name]
-  replace_regex = Regexp.escape(params[:replace])
+  replace_regex = Regexp.new(params[:replace])
 
 	ruby_block "#{params[:name]}" do
 		block do
 			file = Chef::Util::FileEdit.new(file_path)
 			file.search_file_replace(
-				/#{replace_regex}/,
+				replace_regex,
 				(params[:with] or '')
 			)
 			file.write_file
 		end
-		only_if { ::File.read(file_path) =~ /#{replace_regex}/ }
+		only_if { ::File.read(file_path) =~ replace_regex }
 		
 		# Notify listener
 		if params[:notifies]
