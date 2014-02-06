75d11082ed082a6ff371073fe41567e0d3ec8ac9
Jens Segers <segers.jens@gmail.com>
2014-02-06 11:33:55 +0100

    Small tweaks and fixed replace_line

--- providers/append.rb
+++ providers/append.rb
@@ -1,22 +1,28 @@
 # Support whyrun
 def whyrun_supported?
-  true
+	true
 end
 
 action :run do
-	converge_by("Append #{@new_resource}") do
-	  file_path = @new_resource.path || @new_resource.name
 
-		ruby_block "#{@new_resource.name}" do
-			block do
-				begin
-					file = ::File.open(file_path, "a")
-					file.puts @new_resource.line
-				ensure
-					file.close
+	file_path = @new_resource.path || @new_resource.name
+
+	# Check if the file already contains the line
+	unless ::File.exists?(file_path) && ::File.read(file_path) =~ /^#{Regexp.escape(@new_resource.line)}$/
+
+		# Append to file
+		converge_by("Append #{@new_resource}") do
+			ruby_block "#{@new_resource.name}" do
+				block do
+					begin
+						file = ::File.open(file_path, "a")
+						file.puts @new_resource.line
+					ensure
+						file.close
+					end
 				end
 			end
-			not_if { ::File.exists?(file_path) && ::File.read(file_path) =~ /^#{Regexp.escape(@new_resource.line)}$/ }
 		end
+
 	end
 end
--- providers/replace.rb
+++ providers/replace.rb
@@ -1,23 +1,26 @@
 # Support whyrun
 def whyrun_supported?
-  true
+	true
 end
 
 action :run do
-	converge_by("Replace #{@new_resource}") do
-	  file_path = @new_resource.path || @new_resource.name
-	  replace_regex = Regexp.new(@new_resource.replace)
 
-		ruby_block "#{@new_resource.name}" do
-			block do
-				file = Chef::Util::FileEdit.new(file_path)
-				file.search_file_replace(
-					replace_regex,
-					(@new_resource.with or '')
-				)
-				file.write_file
+	file_path = @new_resource.path || @new_resource.name
+	match_regex = Regexp.new(@new_resource.replace)
+
+	# Check if file matches the regex
+	if ::File.read(file_path) =~ match_regex
+
+		# Replace the matching text
+		converge_by("Replace #{@new_resource}") do
+			ruby_block "#{@new_resource.name}" do
+				block do
+					file = Chef::Util::FileEdit.new(file_path)
+					file.search_file_replace(match_regex, (@new_resource.with or ''))
+					file.write_file
+				end
 			end
-			only_if { ::File.read(file_path) =~ replace_regex }
 		end
+
 	end
 end
