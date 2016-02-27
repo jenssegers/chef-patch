68752844cea66b6490a60bb1226be6e4bff9e5ba
Jens Segers <segers.jens@gmail.com>
2016-02-27 17:14:47 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          'MIT'
 description      'Some handy Chef resources for when you want to append, replace or delete and lines in files.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.0'
+version          '2.2.1'
 source_url       'https://github.com/jenssegers/chef-patch' if respond_to?(:source_url)
 issues_url       'https://github.com/jenssegers/chef-patch/issues' if respond_to?(:issues_url)
 
