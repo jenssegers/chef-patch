8a169fb6f9af713564195bbb830ee740d55b55a8
Tim Smith <tsmith@chef.io>
2018-03-06 12:47:06 -0800

    Remove Chef 11 only compatibility in issues_url and source_url
    Mildly modern Chef doesn't require this anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'MIT'
 description      'Some handy Chef resources for when you want to append, replace or delete and lines in files.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.2.1'
-source_url       'https://github.com/jenssegers/chef-patch' if respond_to?(:source_url)
-issues_url       'https://github.com/jenssegers/chef-patch/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/jenssegers/chef-patch'
+issues_url       'https://github.com/jenssegers/chef-patch/issues'
 
 %w(amazon centos debian fedora redhat scientific ubuntu).each do |os|
 	supports os
